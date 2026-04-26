3ab63ea48788c2541b8b56dd24e3e353d70d175b
jayfeather233 <jayfeather233@gmail.com>
2026-04-26 12:25:36 +0800

    init

---
+++ functions/getJapaneseImage/Anime_Img.h
@@ -0,0 +1,10 @@
+#include "processable.h"
+
+class AnimeImg : public processable {
+public:
+    void process(std::string message, const msg_meta &conf);
+    bool check(std::string message, const msg_meta &conf);
+    std::string help();
+};
+
+DECLARE_FACTORY_FUNCTIONS_HEADER
